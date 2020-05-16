cdf36f8f46b5506ae8c99d8040c015cb562f71b3
jecontreras <joseeduar147@gmail.com>
2020-05-15 22:04:07 -0500

    cambios nuevos

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -2,11 +2,11 @@ export const GLOBAL = {
 // actividad-admin: 5e1603065728a31e8d3fbb1e
 
   /* ------------------- Variables de desarrollo  ------------- */
-   url: 'http://localhost:1337/',
-   urlFront: 'http://localhost:4200/',
-   estadoPruebaPagos: true,
-   keyEpayco: '622948e11f5c849086ae425e6d685741',
-   estadoApp: 'Dev',
+  //  url: 'http://localhost:1337/',
+  //  urlFront: 'http://localhost:4200/',
+  //  estadoPruebaPagos: true,
+  //  keyEpayco: '622948e11f5c849086ae425e6d685741',
+  //  estadoApp: 'Dev',
 
 
 
@@ -29,11 +29,11 @@ export const GLOBAL = {
    keyEpayco: '622948e11f5c849086ae425e6d685741',
    estadoApp: 'Prod', */
 
-  //  urlFront: 'https://publihazclick.com/',
-  //  url: 'https://backpublihazclick.herokuapp.com/',
-  //  estadoPruebaPagos: false,
-  //  keyEpayco: '622948e11f5c849086ae425e6d685741',
-  //  estadoApp: 'Prod',
+   urlFront: 'https://publihazclick.com/',
+   url: 'https://backpublihazclick.herokuapp.com/',
+   estadoPruebaPagos: false,
+   keyEpayco: '622948e11f5c849086ae425e6d685741',
+   estadoApp: 'Prod',
 
 
    version: '1.8'
